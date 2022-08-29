a97d4bd7bd323b86535872f40837a877e43a4564
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-08-29 15:57:55 -0500

    cut repetative code

--- index.js
+++ index.js
@@ -7,43 +7,47 @@ const removeLast = document.getElementById("shift-btn");
 
 const myEmojis = ["👩‍💻", "🐶", "⚽"]
 
-for (let i = 0; i < myEmojis.length; i++) {
-    emojisContainer.innerHTML += myEmojis[i]
+function renderEmojis() {
+    emojisContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        emojisContainer.innerHTML += myEmojis[i]
+    }
 }
+renderEmojis()
+
+
 
 
 addBtn.addEventListener("click", function () {
-
-    myEmojis.push(input.value)
-    for (let i = 0; i < myEmojis.length; i++) {
-        emojisContainer.innerHTML = myEmojis
+    if (input.value) {
+        myEmojis.push(input.value)
     }
     input.value = ""
+    renderEmojis()
 })
 
 addbegingn.addEventListener("click", function () {
-
-    myEmojis.unshift(input.value)
-    for (let i = 0; i < myEmojis.length; i++) {
-        emojisContainer.innerHTML = myEmojis
+    if (input.value) {
+        myEmojis.unshift(input.value)
     }
     input.value = ""
+    renderEmojis()
 })
 
 removeFirts.addEventListener("click", function () {
 
-    myEmojis.pop()
-    for (let i = 0; i < myEmojis.length; i++) {
-        emojisContainer.innerHTML = myEmojis
+    if (input.value) {
+        myEmojis.pop(input.value)
     }
     input.value = ""
+    renderEmojis()
 })
 
 removeLast.addEventListener("click", function () {
 
-    myEmojis.shift()
-    for (let i = 0; i < myEmojis.length; i++) {
-        emojisContainer.innerHTML = myEmojis
+    if (input.value) {
+        myEmojis.shift(input.value)
     }
     input.value = ""
+    renderEmojis()
 })
